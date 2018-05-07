6b840fbf82a172653628b991bbba68c770e35686
Fabio <open-dynaMIX@users.noreply.github.com>
2018-05-07 20:06:50 +0200

    Use base64 decoding from lua socket Previously the function used for this was taken from the lua wiki and is under the LGPL. This was a mistake as we don't comply with the LGPL.

--- webui.lua
+++ webui.lua
@@ -1,6 +1,7 @@
-local socket = require("socket")
 require 'mp.options'
 require 'mp.msg'
+local socket = require("socket")
+local dec64 = require("mime").decode("base64")
 
 local msg_prefix = "[webui] "
 
@@ -208,22 +209,6 @@ local function read_file(path)
     local content = file:read "*a"
     file:close()
     return content
-end
-
-local function dec64(data)
-  local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-  data = string.gsub(data, '[^'..b..'=]', '')
-  return (data:gsub('.', function(x)
-    if (x == '=') then return '' end
-      local r,f='',(b:find(x)-1)
-      for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-      return r;
-  end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-    if (#x ~= 8) then return '' end
-    local c=0
-    for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-    return string.char(c)
-  end))
 end
 
 local function log_line(request, code, length)
